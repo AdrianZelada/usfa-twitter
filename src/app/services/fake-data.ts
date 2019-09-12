9d9c6136a297c63c97e2ab2d180a3fe07dcaee66
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-09-12 01:08:59 -0400

    add twitter service, fake-data, twitter-data

---
+++ src/app/services/fake-data.ts
@@ -0,0 +1,13 @@
+export interface TwitterProfile {
+  created_at: string;
+  id: string;
+  text: string;
+}
+
+export const UserProfile: Array<TwitterProfile> = [
+  {
+    created_at: 'asdfasdfas',
+    id: 'afasdfasd',
+    text: 'asdfasfasdfsadfa'
+  }
+];
