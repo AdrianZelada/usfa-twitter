ad35aeda21334ccc878aa6f6b244ff2722244763
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-09-13 01:19:15 -0400

    remove jquery, ngclass

--- src/app/card-preview/card-preview.component.ts
+++ src/app/card-preview/card-preview.component.ts
@@ -8,11 +8,30 @@ import {UserTwitter} from '../services/users-twitter';
 })
 export class CardPreviewComponent implements OnInit {
   @Input() user: any = {};
+  icons: any = {
+    menu: 'fa fa-bars',
+    load: 'fa fa-spin-fast',
+    arrow: 'fa fa-arrow-left',
+  };
+  iconState: string = 'menu';
 
-
+  showContent: boolean =false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggle() {
+    this.showContent = !this.showContent;
+    this.iconState = 'load';
+    if (this.showContent) {
+      setTimeout(() => {
+        this.iconState = 'arrow';
+      }, 800);
+    } else {
+      setTimeout(() => {
+        this.iconState = 'menu';
+      }, 800);
+    }
+  }
 }
--- src/app/services/users-twitter.ts
+++ src/app/services/users-twitter.ts
@@ -127,5 +127,128 @@ export const UserTwitter = [
     "follow_request_sent": null,
     "notifications": null,
     "translator_type": "none"
+  },
+  {
+    "id": 832555380114808832,
+    "id_str": "832555380114808832",
+    "name": "Guillermo David",
+    "screen_name": "_GD_DEV_",
+    "location": "Bolivia",
+    "profile_location": {
+      "id": "c4320f726d222937",
+      "url": "https:\/\/api.twitter.com\/1.1\/geo\/id\/c4320f726d222937.json",
+      "place_type": "unknown",
+      "name": "Bolivia",
+      "full_name": "Bolivia",
+      "country_code": "",
+      "country": "",
+      "contained_within": [],
+      "bounding_box": null,
+      "attributes": {}
+    },
+    "description": "Software Engineer | Geek | React Developer | Founder React Bolivia",
+    "url": "https:\/\/t.co\/1q594OLBSZ",
+    "entities": {
+      "url": {
+        "urls": [
+          {
+            "url": "https:\/\/t.co\/1q594OLBSZ",
+            "expanded_url": "https:\/\/guillermoparedes.github.io\/blog\/",
+            "display_url": "guillermoparedes.github.io\/blog\/",
+            "indices": [
+              0,
+              23
+            ]
+          }
+        ]
+      },
+      "description": {
+        "urls": []
+      }
+    },
+    "protected": false,
+    "followers_count": 147,
+    "friends_count": 376,
+    "listed_count": 3,
+    "created_at": "Fri Feb 17 11:40:45 +0000 2017",
+    "favourites_count": 742,
+    "utc_offset": null,
+    "time_zone": null,
+    "geo_enabled": true,
+    "verified": false,
+    "statuses_count": 544,
+    "lang": null,
+    "status": {
+      "created_at": "Fri Sep 13 00:52:57 +0000 2019",
+      "id": 1172312174116687872,
+      "id_str": "1172312174116687872",
+      "text": "Amigos de Cbba, no se pierdan el primer encuentro de #FacebookDeveloperCircleCbba https:\/\/t.co\/KqW0AgsBLp",
+      "truncated": false,
+      "entities": {
+        "hashtags": [
+          {
+            "text": "FacebookDeveloperCircleCbba",
+            "indices": [
+              53,
+              81
+            ]
+          }
+        ],
+        "symbols": [],
+        "user_mentions": [],
+        "urls": [
+          {
+            "url": "https:\/\/t.co\/KqW0AgsBLp",
+            "expanded_url": "https:\/\/twitter.com\/carlos_olivera\/status\/1172302081543331841",
+            "display_url": "twitter.com\/carlos_olivera…",
+            "indices": [
+              82,
+              105
+            ]
+          }
+        ]
+      },
+      "source": "<a href=\"http:\/\/twitter.com\/download\/android\" rel=\"nofollow\">Twitter for Android<\/a>",
+      "in_reply_to_status_id": null,
+      "in_reply_to_status_id_str": null,
+      "in_reply_to_user_id": null,
+      "in_reply_to_user_id_str": null,
+      "in_reply_to_screen_name": null,
+      "geo": null,
+      "coordinates": null,
+      "place": null,
+      "contributors": null,
+      "is_quote_status": true,
+      "quoted_status_id": 1172302081543331841,
+      "quoted_status_id_str": "1172302081543331841",
+      "retweet_count": 0,
+      "favorite_count": 1,
+      "favorited": false,
+      "retweeted": false,
+      "possibly_sensitive": false,
+      "lang": "es"
+    },
+    "contributors_enabled": false,
+    "is_translator": false,
+    "is_translation_enabled": false,
+    "profile_background_color": "000000",
+    "profile_background_image_url": "http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png",
+    "profile_background_image_url_https": "https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png",
+    "profile_background_tile": false,
+    "profile_image_url": "http:\/\/pbs.twimg.com\/profile_images\/1166380557980377091\/jpY2klUR.jpg",
+    "profile_image_url_https": "https:\/\/pbs.twimg.com\/profile_images\/1166380557980377091\/jpY2klUR_normal.jpg",
+    "profile_banner_url": "https:\/\/pbs.twimg.com\/profile_banners\/832555380114808832\/1566921772",
+    "profile_link_color": "ABB8C2",
+    "profile_sidebar_border_color": "000000",
+    "profile_sidebar_fill_color": "000000",
+    "profile_text_color": "000000",
+    "profile_use_background_image": false,
+    "has_extended_profile": true,
+    "default_profile": false,
+    "default_profile_image": false,
+    "following": null,
+    "follow_request_sent": null,
+    "notifications": null,
+    "translator_type": "none"
   }
 ]
